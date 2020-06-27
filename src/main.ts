a2ec11c10849fb348614a00bbcb67b90114571ac
Sorami Hisamoto <s@89.io>
2020-06-27 21:41:59 +0900

    Read dictionary binary file!

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+const fs = require('fs');
+
+fs.readFile('system.dic', (err, content) => {
+    if (err) {
+        console.error(err);
+    }
+
+    let buf = Buffer.from(content);
+    let version = buf.readBigUInt64LE(0);
+    let createTime = buf.readBigInt64LE(64);
+
+    console.log(`version: 0x${version.toString(16)}`);
+    console.log(`create time: ${createTime}`);
 });
 
-rl.on('line', (line) => {
-  console.log(line);
-}).on('close', () => {
-  process.exit(0);
-});
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.on('line', (line) => {
+//   console.log(line);
+// }).on('close', () => {
+//   process.exit(0);
+// });
