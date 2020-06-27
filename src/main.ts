5b0b71ed32b1f281688d4d3a5254ccf57b5ad6d2
Sorami Hisamoto <s@89.io>
2020-06-27 21:23:20 +0900

    First commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (line) => {
+  console.log(line);
+}).on('close', () => {
+  process.exit(0);
+});
